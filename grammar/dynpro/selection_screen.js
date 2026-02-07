abc4cc57332c08454b8a13c9b156ea41b69a828b
Kenny <106347478+kennyhml@users.noreply.github.com>
2026-02-07 21:37:36 +0100

    feat: add dynpro function keys

--- grammar/dynpro/selection_screen.js
+++ grammar/dynpro/selection_screen.js
@@ -20,14 +20,17 @@ export default {
         $.end_of_screen_element,
         $.begin_of_block_element,
         $.end_of_block_element,
+        $.begin_of_tabbed_block_element,
         $.horizontal_line_element,
         $.comment_element,
         $.pushbutton_element,
-        $.begin_of_line_element,
-        $.end_of_line_element,
+        $.tab_element,
 
         $.blank_line_directive,
+        $.begin_of_line_directive,
+        $.end_of_line_directive,
         $.screen_position_directive,
+        $.function_key_directive
     ),
 
     // https://help.sap.com/doc/abapdocu_latest_index_htm/latest/en-US/ABAPSELECTION-SCREEN_NORMAL.html
@@ -51,6 +54,30 @@ export default {
         ))
     ),
 
+    // https://help.sap.com/doc/abapdocu_latest_index_htm/latest/en-US/ABAPSELECTION-SCREEN_TABBED.html
+    begin_of_tabbed_block_element: $ => seq(
+        ...kws("begin", "of", "tabbed", "block"),
+        field("block", $.identifier),
+        field("lines", $.tab_lines_spec),
+        optional(field("intervals", $.no_intervals_spec)),
+    ),
+
+    /**
+     * Tab element (button) of a tabbed block.
+     * 
+     * Refer to {@link begin_of_tabbed_block_element}
+     */
+    tab_element: $ => seq(
+        kw("tab"),
+        "(", field("length", $._immediate_number), token.immediate(")"),
+        field("name", $.identifier),
+        field("user_command", $.user_command_spec),
+        repeat(choice(
+            field("default", $.default_tab_screen_spec),
+            field("modif_id", $.modif_id_spec),
+        ))
+    ),
+
     /**
      * Closes a {@link begin_of_screen_element} or {@link begin_of_subscreen_statement}
      */
@@ -69,6 +96,9 @@ export default {
         ))
     ),
 
+    /**
+     * Closes a {@link begin_of_block_element} or {@link begin_of_tabbed_block_element}.
+     */
     end_of_block_element: $ => seq(
         ...kws("end", "of", "block"),
         field("block", $.identifier),
@@ -114,14 +144,25 @@ export default {
     ),
 
     // https://help.sap.com/doc/abapdocu_latest_index_htm/latest/en-US/ABAPSELECTION-SCREEN_LINE.html
-    begin_of_line_element: _ => seq(...kws("begin", "of", "line")),
+    begin_of_line_directive: _ => seq(...kws("begin", "of", "line")),
 
     screen_position_directive: $ => seq(
         kw("position"),
         field("position", $.output_position_spec),
     ),
 
-    end_of_line_element: _ => seq(...kws("end", "of", "line")),
+    /**
+     * Since this only actives an existing element (key 0 to 5) this should
+     * be considered a directive rather than an element.
+     * 
+     * https://help.sap.com/doc/abapdocu_latest_index_htm/latest/en-US/ABAPSELECTION-SCREEN_FUNCTIONKEY.html
+     */
+    function_key_directive: $ => seq(
+        ...kws("function", "key"),
+        field("number", $.number)
+    ),
+
+    end_of_line_directive: _ => seq(...kws("end", "of", "line")),
 
     for_select_option_spec: $ => seq(
         optional(field("text", $.__element_text)),
@@ -146,6 +187,22 @@ export default {
     nesting_level_spec: $ => seq(
         ...kws("nesting", "level"),
         field("level", $.number)
+    ),
+
+    tab_lines_spec: $ => seq(
+        kw("for"),
+        field("number", $.number),
+        kw("lines")
+    ),
+
+    default_tab_screen_spec: $ => seq(
+        kw("default"),
+        optional(seq(
+            kw("program"),
+            field("program", $.identifier)
+        )),
+        kw("screen"),
+        field("dynnr", $.number)
     ),
 
     window_spec: _ => seq(...kws("as", "window")),
