3b133b45cf2da0e9fbdaf0f09f3ec5a03277e126
Kenny <106347478+kennyhml@users.noreply.github.com>
2026-02-14 20:43:30 +0100

    refactor: move case type of statement to control

--- grammar/core/control/index.js
+++ grammar/core/control/index.js
@@ -3,6 +3,7 @@ import if_statement_rules from './if_statement.js'
 import while_statement_rules from './while_statement.js'
 import try_statement_rules from './try_statement.js'
 import case_statement_rules from './case_statement.js'
+import case_type_statement_rules from './case_type_statement.js'
 
 export default {
 
@@ -13,7 +14,7 @@ export default {
         $.while_statement,
 
         $.case_statement,
-        $.type_case_statement,
+        $.case_type_of_statement,
         $.do_statement,
     ),
 
@@ -21,5 +22,6 @@ export default {
     ...if_statement_rules,
     ...while_statement_rules,
     ...try_statement_rules,
-    ...case_statement_rules
+    ...case_statement_rules,
+    ...case_type_statement_rules
 }
