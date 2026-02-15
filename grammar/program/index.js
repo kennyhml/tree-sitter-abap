8d565e25e807ea926ce6381de9e659d3aee663d9
Kenny <106347478+kennyhml@users.noreply.github.com>
2026-02-15 17:40:39 +0100

    refactor: extract forms

--- grammar/program/index.js
+++ grammar/program/index.js
@@ -1,6 +1,7 @@
 import report_statement_rules from './report_statement.js'
 import include_statement_rules from './include_statement.js'
 import tables_declaration_rules from './tables_declaration.js'
+import form_rules from './forms.js'
 
 export default {
 
@@ -8,9 +9,11 @@ export default {
         $.report_statement,
         $.include_statement,
         $.tables_declaration,
+        $.form_definition
     ),
 
     ...report_statement_rules,
     ...include_statement_rules,
     ...tables_declaration_rules,
+    ...form_rules,
 }
