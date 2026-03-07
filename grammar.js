6c6d712ddf907935389eb9eb7c257ca378d8ab85
Kenny <106347478+kennyhml@users.noreply.github.com>
2026-03-07 20:07:13 +0100

    refactor: extract ref expression

--- grammar.js
+++ grammar.js
@@ -573,21 +573,7 @@ module.exports = grammar({
 
 
 
-    /**
-     * https://help.sap.com/doc/abapdocu_latest_index_htm/latest/en-US/ABENCONSTRUCTOR_EXPRESSION_REF.html
-     */
-    ref_expression: $ => seq(
-      gen.kw("ref"),
-      field("type", $._constructor_result),
-      "(",
-      optional($.let_expression),
-      choice(
-        field("dobj", $.data_object),
-        field("tab_expr", $.table_expression)
-      ),
-      optional($._table_expr_default),
-      ")"
-    ),
+
 
     /**
      * Branches into multiple "forms".
