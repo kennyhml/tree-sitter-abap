051e4c3bece56ab3e5b0feaab27bd8871f4c780b
kennyhml <kendrick.hommel@gmail.com>
2026-08-08 20:33:01 +0200

    fix: pragmas dont interrupt chainings

--- grammar/elements/directives/pragmas.js
+++ grammar/elements/directives/pragmas.js
@@ -7,16 +7,16 @@ module.exports = {
   pragma: $ =>
     seq(
       "##",
-      field("code", alias(token.immediate(/[^\n\r#. ]+/), $.identifier)),
+      field("code", alias(token.immediate(/[^\n\r#., ]+/), $.identifier)),
       // Up to 2 parameters are possible, but extras dont allow optionals.
       // While this hack does work fine, it unfortunately causes the parameter nodes
       // to always show up in the tree even when no parameter are specified.
       // So im not sure if I want to have it that way.
       /\[?/,
-      field("parameter", alias(token.immediate(/[^\n\r\].]*/), $.identifier)),
+      field("parameter", alias(token.immediate(/[^\n\r\].,]*/), $.identifier)),
       /\]?/,
       /\[?/,
-      field("parameter", alias(token.immediate(/[^\n\r\].]*/), $.identifier)),
+      field("parameter", alias(token.immediate(/[^\n\r\].,]*/), $.identifier)),
       /\]?/,
     ),
 };
